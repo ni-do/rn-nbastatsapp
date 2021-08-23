cf78e6e31e861656f6e08201402614278fd84431
Niklas Donath <niklas.donath@web.de>
2021-08-23 22:05:51 +0200

    moved logo info to separate js

---
+++ screens/Logo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Image, StyleSheet } from 'react-native'
+
+const imagePath = './../assets/NBAStatsLogo.png'
+const imageSrc = require(imagePath)
+
+const styles = StyleSheet.create({
+    tinyLogo: {
+        width: 50,
+        height: 50,
+    },
+    logo: {
+        width: 320,
+    },
+})
+
+const Logo = (props) => {
+    return (
+        <Image style={styles.logo} source={imageSrc} />
+    )
+}
+
+export default Logo
