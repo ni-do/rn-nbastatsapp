625387191eeba51ebede73cbab8c6fca1d74297a
Niklas Donath <niklas.donath@web.de>
2021-12-28 13:52:21 +0100

    added date-fns, formatted last game card

--- stacks/main/GameCard.js
+++ stacks/main/GameCard.js
@@ -1,13 +1,32 @@
 import React from "react"
-import { Text, View } from "react-native"
+import { Text, View, StyleSheet } from "react-native"
+import { format } from 'date-fns'
+
+const styles = StyleSheet.create({
+  gameDate: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  teamNames: {
+    textAlign: 'center',
+  },
+  gameResult: {
+    fontSize: 24,
+    textAlign: 'center',
+  }
+})
 
 const GameCard = ({ game }) => {
   console.log('GameCard game:', game)
+
+  const gameDate = new Date(game.date)
+  const formattedGameDate = format(gameDate, 'dd.MM.yyyy')
+
   return (
     <View>
-      <Text>{game.date}</Text>
-      <Text>{game.home_team.name} - {game.visitor_team.name}</Text>
-      <Text>{game.home_team_score} : {game.visitor_team_score}</Text>
+      <Text style={styles.gameDate}>{formattedGameDate}</Text>
+      <Text style={styles.teamNames}>{game.home_team.name} - {game.visitor_team.name}</Text>
+      <Text style={styles.gameResult}>{game.home_team_score} : {game.visitor_team_score}</Text>
     </View>
   )
 }
